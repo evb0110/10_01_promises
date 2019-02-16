547e336820b2c4ca9cfa128a4117d79c70edd908
Eugene Barsky <34789976+evb0110@users.noreply.github.com>
2019-02-16 22:04:49 +0300

    first test works

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,37 +1,13 @@
-class GameSavingData {
-  constructor(data) {
-    this.data = data;
-  }
+import GameSavingData from './GameSavingData';
+import readGameSaving from './readGameSaving';
 
-  json() {
-    return new Promise((resolve, reject) => {
-      // эмуляция обработки ArrayBuffer
-      setTimeout(() => {
-        resolve(String.fromCharCode.apply(null, new Uint16Array(this.data)));
-      }, 1000);
-    });
+export default class GameSavingLoader {
+  static load() {
+    return readGameSaving()
+      .then(data => new GameSavingData(data))
+      .catch(err => console.error(`reading error: ${err}`))
+      .then(data => data.json())
+      .catch(err => console.error(`json converting error: ${err}`));
   }
 }
 
-function readGameSaving() {
-  return new Promise((resolve, reject) => {
-    // эмуляция чтения файла
-    setTimeout(() => {
-      const data =
-        '{"id":9,"created":1546300800,"userInfo":{"id":1,name":"Hitman","level":10,"points":2000}}';
-      return (input => {
-        const buffer = new ArrayBuffer(input.length * 2);
-        const bufferView = new Uint16Array(buffer);
-        for (let i = 0; i < input.length; i++) {
-          bufferView[i] = input.charCodeAt(i);
-        }
-        resolve(buffer);
-      })(data);
-    }, 5000);
-  });
-}
-
-readGameSaving()
-  .then(data => new GameSavingData(data))
-  .then(data => data.json())
-  .then(console.log);
