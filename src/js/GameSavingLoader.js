08ab937b9cb698b3c08195cab81258bc354b3b38
evb0110 <7149553@gmail.com>
2019-02-18 01:47:29 +0300

    still stuck with 2nd test

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,13 +1,13 @@
-import GameSavingData from './GameSavingData';
-import readGameSaving from './readGameSaving';
+import GameSavingData from "./GameSavingData";
+import readGameSaving from "./readGameSaving";
 
 export default class GameSavingLoader {
   static load() {
     return readGameSaving()
+      .catch(err => {
+        throw new Error(err);
+      })
       .then(data => new GameSavingData(data))
-      .catch(err => console.error(`reading error: ${err}`))
-      .then(data => data.json())
-      .catch(err => console.error(`json converting error: ${err}`));
+      .then(data => data.json());
   }
 }
-
